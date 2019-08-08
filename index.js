5d318cc0c535837c8cd19f5659f3d4f273f9becf
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-08-08 13:52:47 +0300

    fix: change function isElementEnabled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver'); 
 let driver = new Builder().forBrowser('chrome').build();
 let el = driver.findElement(By.id('orb-search-q')); //'orb-search-q'
-let navLink = driver.findElement(By.css('#orb-nav-links>ul>li.orb-nav-newsdotcom'));
+let navLink = driver.findElement(By.css('#orbtytytthth')); //'#orb-nav-links>ul>li.orb-nav-newsdotcom'
 
 async function isElementDisplayed(elem, time) {
     while (elem < time || elem !== true) {
@@ -27,31 +27,39 @@ async function searchElement() {
     driver.quit();
 };
 
-searchElement();
-
-async function isElementEnabled(elem, time) {
-    while (elem < time || elem !== true) {
+async function isElementEnabled(elem, time) { 
+    let startTime = new Date().getMilliseconds(); 
+    let endTime;
+    let diff;
+    while (diff < time || elem !== true) {
         try {
-            if (await elem.isEnabled()) {
+            let getEl = await elem.isEnabled();
+            endTime = new Date().getMilliseconds();             
+            diff = (endTime - startTime); 
+            if(time < diff) {
+                console.log('-> Sorry, time is over...', diff);
+                return false; 
+            }
+            if (getEl) {
                 console.log('-> Element is enabled');
+                console.log(endTime, startTime, diff);
                 return elem; 
-            } 
-        }
-        catch(err) {       
+            }
+                    
+        } catch(err) {
             console.log('-> Element is not enabled: ' + err);
             return false;
-        }
+        }        
     } return elem;
 };
 
-
 async function getNavLink() {
     await driver.get('https://www.bbc.com/');
-    await isElementEnabled(navLink, 5).then(el => {
+    await isElementEnabled(navLink, 10000).then(el => {
         el.click();
     })
     await driver.wait(until.titleIs('Home - BBC News'), 10000);
     driver.quit();
 };
 
-// getNavLink();
+getNavLink();
